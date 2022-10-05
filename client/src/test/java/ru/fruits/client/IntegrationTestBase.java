d10edcc7b1cb8c756cb3c994dc6203ab814f308a
abychkov <abychkov@cinimex.ru>
2022-10-05 17:06:28 +0300

    Added tests for Client service

--- client/src/test/java/ru/fruits/client/IntegrationTestBase.java
+++ client/src/test/java/ru/fruits/client/IntegrationTestBase.java
@@ -3,12 +3,14 @@ package ru.fruits.client;
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 import ru.fruits.client.initializer.Mysql;
 
 @SpringBootTest
 @ContextConfiguration(initializers = {
     Mysql.Initializer.class
 })
+@Transactional
 public abstract class IntegrationTestBase {
 
     @BeforeAll
---
+++ client/src/test/java/ru/fruits/client/resources/rest/OrdersControllerTest.java
@@ -0,0 +1,40 @@
+package ru.fruits.client.resources.rest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import ru.fruits.client.IntegrationTestBase;
+import ru.fruits.client.entity.Order;
+
+@Sql("/sql/data.sql")
+@AutoConfigureMockMvc
+class OrdersControllerTest extends IntegrationTestBase {
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    @SneakyThrows
+    void getOrders() {
+        MvcResult response = mockMvc.perform(get("/api/v1/orders/")
+                .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andReturn();
+
+        Order[] orders = objectMapper.readValue(response.getResponse().getContentAsString(), Order[].class);
+
+        assertEquals(2, orders.length);
+        assertEquals("test_order_2", orders[1].getName());
+    }
+}
